b918c1d46effdff7f06107eded476825a06ca3cd
Алексей <alexleskov2000@gmail.com>
2021-05-23 12:59:15 +0800

    Сделать поля Имя и Фамилия обязательными

--- registration/forms.py
+++ registration/forms.py
@@ -3,6 +3,12 @@ from . import models
 
 
 class UserRegistrationForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+
+        for key in self.fields:
+            self.fields[key].required = True
+
     class Meta:
         model = models.CustomUser
         fields = ('first_name', 'last_name', 'division_code_gosb', 'division_code_vsp', 'post')
